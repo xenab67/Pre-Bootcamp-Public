a5bb56038b106b789bc92aa6fbd8d7122990461a
Xena Ball <xenaball67@gmail.com>
2021-08-29 11:50:36 -0500

    initial commit

---
+++ conditions/loops.js
@@ -0,0 +1,28 @@
+for (var i=1; i<=20; i++) {
+    if (i%2!=0)
+        console.log(i);
+}
+
+
+for (var i=100; i>=0; i--){
+    if (i%3==0)
+    console.log(i);
+}
+
+
+for (var i=4; i>-4; i-=1.5){
+    console.log(i);
+}
+
+
+var sum=0;
+for (var i=1; i<=100; i++){
+    sum +=i;
+    console.log(sum);
+}
+
+var product=1;
+for (var i=1; i<=12; i++){
+    product *=i;
+    console.log(product);
+}
